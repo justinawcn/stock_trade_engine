10e8d4564f8425d43e8ba4b53d83766b6aab45a6
Justina Wu <wuchihning@dhcp-v094-141.mobile.uci.edu>
2025-03-06 14:20:49 -0800

    infrastructure

--- Engine.py
+++ Engine.py
@@ -1,9 +1,35 @@
-public class Engine {
-    static void main(String[] args) {
-        System.out.println("market starts...");
-    }
+from enum import enum
+
+class Order:
+    def __init__(self, OrderType, TickerSymbol, Qty, Price):
+        self.OrderType = OrderType
+        self.TickerSymbol = TickerSymbol
+        self.Qty = Qty
+        self.Price = Price
+
+class OrderType(enum):
+    BUY = True
+    SELL = False
+
+class OrderBook():
+    def __init__(self):
+        self.buyOrders = linkedlist
+        self.sellOrders = linkedlist
+
+
+def addOrder(OrderType: OrderType, TickerSymbol: Type, Qty: Int, Price: Float) {
+
+}
+
+def matchOrder(TickerSymbol) { 
+    # O(n)
+
+}
+
+def stimulator() {
+
 }
 
 def main() {
-    print("market starts...");
+    print("market starts...")
 }
---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestAddOrder(unittest.TestCase):
+
+    def test_list_structure(self):
+        
+        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
