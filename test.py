1d582b46b41aaa1ba275c9a51ee72362d5b901ad
justinawcn <125609958+justinawcn@users.noreply.github.com>
2025-03-06 17:17:14 -0800

    addOrder(buy) test

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import unittest
+from Engine import OrderBook
 
 class TestAddOrder(unittest.TestCase):
+    orderBook = OrderBook()
 
-    def test_list_structure(self):
-        
-        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+    def test_buy_simple_add(self):
+        OrderBook.addOrder("BUY", "ticker1", 100, 15.0)
+        order1 = Order("BUY", "ticker1", 100, 15.0)
+        self.assertEqual(orderBook.buyOrders, order1, 'the first buy order insert wrongly.')
+
+    def test_buy_cheaper_add(self):
+
+    
+    def test_buy_greater_add(self):
 
     
 
