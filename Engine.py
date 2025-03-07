48f0857e9e71b3e342defa8f1e6d9437a0524f10
justinawcn <125609958+justinawcn@users.noreply.github.com>
2025-03-06 17:16:37 -0800

    doubly_linkedlist buy

--- Engine.py
+++ Engine.py
@@ -6,25 +6,67 @@ class Order:
         self.TickerSymbol = TickerSymbol
         self.Qty = Qty
         self.Price = Price
+        self.prev = None
+        self.next = None
+
+
 
 class OrderType(enum):
     BUY = True
     SELL = False
 
+
+
+class TickerType:
+    def __init__(self, symbol: Union[int, str]):
+        if isinstance(symbol, int):
+            if not (0 <= symbol < 1024):
+                raise ValueError("Integer ticker symbols outside the bound, 0 - 1023")
+        elif not isinstance(symbol, str):
+            raise TypeError("Ticker symbol isn't an integer or a string which is incorrect")
+        self.symbol = symbol
+
+    def __str__(self):
+        return str(self.symbol)
+
+
+
+
 class OrderBook():
+    BUY = 0
+    SELL = 1
+
+
     def __init__(self):
-        self.buyOrders = linkedlist
-        self.sellOrders = linkedlist
+        self.buyOrders = None # descending order
+        self.sellOrders = None # ascending order
 
 
-def addOrder(OrderType: OrderType, TickerSymbol: Type, Qty: Int, Price: Float) {
+    def addOrder(self, orderType: String, tickerSymbol: TickerType, qty: Int, price: Float) {
+        added_order = Order(orderType, tickerSymbol, qty, price)
 
-}
+        if orderType == "BUY":
+            
+            if self.buyOrders is None:
+                self.buyOrders = added_order
+            else:
+                # traversing the buy list and store in a descending order
+                ptr = self.buyOrders
+                while (ptr.price > added_order.price):
+                    previous_order = ptr
+                    ptr = ptr.next
+                # current order price is no longer greater than the order to be added
+                # the order should be added before the current order
+                previous_order.next = added_order
+                added_order.next = ptr
+                ptr.prev = added_order
 
-def matchOrder(TickerSymbol) { 
-    # O(n)
+    }
 
-}
+    def matchOrder(self, TickerSymbol) { 
+        # O(n)
+
+    }
 
 def stimulator() {
 
@@ -32,4 +74,5 @@ def stimulator() {
 
 def main() {
     print("market starts...")
+    stockMarket = new OrderBook
 }
