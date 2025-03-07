1fe55eeda48609fae0207c2d6540d7a66e971d0e
Justina Wu <wuchihning@JustinadeMacBook-Pro.local>
2025-03-07 09:47:03 -0800

    fixing removing matched order

--- Engine.py
+++ Engine.py
@@ -57,7 +57,7 @@ class OrderBook():
                     self.buyOrders.prev = added_order
                     added_order.next = self.buyOrders
                     self.buyOrders = added_order
-                    return
+                    return self.matchOrder(added_order.TickerSymbol)
                 
                 ptr = self.buyOrders
                 previous_order = self.buyOrders
@@ -71,6 +71,7 @@ class OrderBook():
                 previous_order.next = added_order
                 added_order.next = ptr
                 ptr.prev = added_order
+                return self.matchOrder(added_order.TickerSymbol)
         elif orderType == "SELL":
             if self.sellOrders is None:
                 self.sellOrders = added_order
@@ -82,7 +83,7 @@ class OrderBook():
                     self.sellOrders.prev = added_order
                     added_order.next = self.sellOrders
                     self.sellOrders = added_order
-                    return 
+                    return self.matchOrder(added_order.TickerSymbol)
                 
                 ptr = self.sellOrders
                 previous_order = self.sellOrders
@@ -94,13 +95,71 @@ class OrderBook():
                 previous_order.next = added_order
                 added_order.next = ptr
                 ptr.prev = added_order
+                return self.matchOrder(added_order.TickerSymbol)
         else:
             raise ValueError("incorrect orderType, neither BUY nor SELL")
 
 
-    # def matchOrder(self, tickerSymbol):
-
+    def matchOrder(self, tickerSymbol):
         # O(n)
+        buy = self.buyOrders
+        sell = self.sellOrders
+        while buy is not None and sell is not None:
+            if buy.TickerSymbol != tickerSymbol:
+                buy = buy.next
+                continue
+            if sell.TickerSymbol != tickerSymbol:
+                sell = sell.next
+                continue
+
+            if buy.Price >= sell.Price:
+                matched_qty = min(buy.Qty, sell.Qty)
+
+                buy.Qty -= matched_qty
+                sell.Qty -= matched_qty
+
+                if buy.Qty == 0:
+                    self._remove_order(OrderType.BUY, buy)
+                    if buy is not None:
+                        buy = buy.next
+
+                if sell.Qty == 0:
+                    print("sell empty")
+                    self._remove_order(OrderType.SELL, sell)
+                    if sell is not None:
+                        sell = sell.next
+                
+            else:
+                break
+
+    def _remove_order(self, OrderType, order):
+        """helper function to remove an order from the linked list"""
+        if OrderType:
+            if self.buyOrders == order:
+                if order is not None:
+                    self.buyOrders = order.next
+                else:
+                    self.buyOrders = None
+            else:
+                if order is not None:
+                    order.prev.next = order.next
+                    order.next.prev = order.prev
+                else:
+                    order.prev.next = None
+        else:
+            if self.sellOrders == order:
+                if order is not None:
+                    self.sellOrders = order.next
+                else:
+                    self.sellOrders = None
+            else:
+                if order is not None:
+                    order.prev.next = order.next
+                    order.next.prev = order.prev
+                else:
+                    order.prev.next = None
+
+
 
 
 
--- test.py
+++ test.py
@@ -92,5 +92,116 @@ class TestAddOrder(unittest.TestCase):
         self.assertEqual(orderBook.sellOrders.Qty, 100, 'the first buy order insert wrongly. (quantity)')
         self.assertEqual(orderBook.sellOrders.Price, 15.0, 'the first buy order insert wrongly. (price)')
 
+    def test_one_no_match(self):
+        orderBook = OrderBook()
+        OrderBook.addOrder(orderBook, "BUY", "ticker1", 100, 50.0)
+        OrderBook.addOrder(orderBook, "SELL", "ticker1", 110, 20.0)
+
+        self.assertEqual(orderBook.buyOrders.OrderType, "BUY", 'buy order after match. (ordertype)')
+        self.assertEqual(orderBook.buyOrders.TickerSymbol, "ticker1", 'buy order after match. (tickersymbol)')
+        self.assertEqual(orderBook.buyOrders.Qty, 100, 'buy order after match. (quantity)')
+        self.assertEqual(orderBook.buyOrders.Price, 50.0, 'buy order after match. (price)')
+
+        self.assertEqual(orderBook.sellOrders.OrderType, "SELL", 'sell order after match. (ordertype)')
+        self.assertEqual(orderBook.sellOrders.TickerSymbol, "ticker1", 'sell order after match. (tickersymbol)')
+        self.assertEqual(orderBook.sellOrders.Qty, 110, 'sell order after match. (quantity)')
+        self.assertEqual(orderBook.sellOrders.Price, 20.0, 'sell order after match. (price)')
+
+    def test_fully_match(self):
+        orderBook = OrderBook()
+        OrderBook.addOrder(orderBook, "BUY", "ticker1", 150, 50.0)
+        OrderBook.addOrder(orderBook, "SELL", "ticker1", 150, 20.0)
+
+        OrderBook.matchOrder(orderBook, "ticker1")
+
+        self.assertEqual(orderBook.buyOrders, None, "buy ticker should be matched and thus gone")
+        self.assertEqual(orderBook.sellOrders, None, "sell ticker should be matched and thus gone")
+
+
+    def test_one_sell_left_match(self):
+        orderBook = OrderBook()
+        OrderBook.addOrder(orderBook, "BUY", "ticker1", 110, 50.0)
+        OrderBook.addOrder(orderBook, "SELL", "ticker1", 150, 20.0)
+
+        OrderBook.matchOrder(orderBook, "ticker1")
+
+        self.assertEqual(orderBook.buyOrders, None, "buy ticker should be matched and thus gone")
+
+        self.assertEqual(orderBook.sellOrders.OrderType, "SELL", 'sell order after match. (ordertype)')
+        self.assertEqual(orderBook.sellOrders.TickerSymbol, "ticker1", 'sell order after match. (tickersymbol)')
+        self.assertEqual(orderBook.sellOrders.Qty, 40, 'sell order after match. (quantity)')
+        self.assertEqual(orderBook.sellOrders.Price, 20.0, 'sell order after match. (price)')
+
+
+    def test_one_buy_left_match(self):
+        orderBook = OrderBook()
+        OrderBook.addOrder(orderBook, "BUY", "ticker1", 150, 50.0)
+        OrderBook.addOrder(orderBook, "SELL", "ticker1", 110, 20.0)
+
+        OrderBook.matchOrder(orderBook, "ticker1")
+
+        self.assertEqual(orderBook.sellOrders, None, "sell ticker should be matched and thus gone")
+
+        self.assertEqual(orderBook.buyOrders.OrderType, "BUY", 'the first buy order insert wrongly. (ordertype)')
+        self.assertEqual(orderBook.buyOrders.TickerSymbol, "ticker1", 'the first s order insert wrongly. (tickersymbol)')
+        self.assertEqual(orderBook.buyOrders.Qty, 40, 'the first buy order insert wrongly. (quantity)')
+        self.assertEqual(orderBook.buyOrders.Price, 50.0, 'the first buy order insert wrongly. (price)')
+
+    # def test_update_buy_head(self):
+    #     orderBook = OrderBook()
+    #     OrderBook.addOrder(orderBook, "BUY", "ticker1", 110, 50.0)
+    #     OrderBook.addOrder(orderBook, "SELL", "ticker1", 150, 20.0)
+    #     OrderBook.addOrder(orderBook, "BUY", "ticker2", 30, 20.0)
+
+    #     OrderBook.matchOrder(orderBook, "ticker1")
+
+    #     self.assertEqual(orderBook.sellOrders, None, "sell ticker should be matched and thus gone")
+
+    #     self.assertEqual(orderBook.buyOrders.OrderType, "BUY", 'the first buy order insert wrongly. (ordertype)')
+    #     self.assertEqual(orderBook.buyOrders.TickerSymbol, "ticker2", 'the first s order insert wrongly. (tickersymbol)')
+    #     self.assertEqual(orderBook.buyOrders.Qty, 30, 'the first buy order insert wrongly. (quantity)')
+    #     self.assertEqual(orderBook.buyOrders.Price, 20.0, 'the first buy order insert wrongly. (price)')
+
+    #     self.assertEqual(orderBook.sellOrders.OrderType, "SELL", 'sell order after match. (ordertype)')
+    #     self.assertEqual(orderBook.sellOrders.TickerSymbol, "ticker1", 'sell order after match. (tickersymbol)')
+    #     self.assertEqual(orderBook.sellOrders.Qty, 40, 'sell order after match. (quantity)')
+    #     self.assertEqual(orderBook.sellOrders.Price, 20.0, 'sell order after match. (price)')
+
+    # def test_update_sell_head(self):
+    #     orderBook = OrderBook()
+    #     OrderBook.addOrder(orderBook, "BUY", "ticker1", 150, 50.0)
+    #     OrderBook.addOrder(orderBook, "SELL", "ticker1", 110, 20.0)
+    #     OrderBook.addOrder(orderBook, "SELL", "ticker2", 30, 20.0)
+
+    #     OrderBook.matchOrder(orderBook, "ticker1")
+
+    #     self.assertEqual(orderBook.sellOrders, None, "sell ticker should be matched and thus gone")
+
+    #     self.assertEqual(orderBook.buyOrders.OrderType, "BUY", 'the first buy order insert wrongly. (ordertype)')
+    #     self.assertEqual(orderBook.buyOrders.TickerSymbol, "ticker1", 'the first s order insert wrongly. (tickersymbol)')
+    #     self.assertEqual(orderBook.buyOrders.Qty, 40, 'the first buy order insert wrongly. (quantity)')
+    #     self.assertEqual(orderBook.buyOrders.Price, 50.0, 'the first buy order insert wrongly. (price)')
+
+    #     self.assertEqual(orderBook.sellOrders.OrderType, "SELL", 'sell order after match. (ordertype)')
+    #     self.assertEqual(orderBook.sellOrders.TickerSymbol, "ticker2", 'sell order after match. (tickersymbol)')
+    #     self.assertEqual(orderBook.sellOrders.Qty, 30, 'sell order after match. (quantity)')
+    #     self.assertEqual(orderBook.sellOrders.Price, 20.0, 'sell order after match. (price)')
+
+    
+    # def test_one_buy_two_sell_match(self):
+    #     orderBook = OrderBook()
+    #     OrderBook.addOrder(orderBook, "BUY", "ticker1", 150, 50.0)
+    #     OrderBook.addOrder(orderBook, "SELL", "ticker1", 110, 20.0)
+    #     OrderBook.addOrder(orderBook, "SELL", "ticker1", 30, 20.0)
+
+    #     OrderBook.matchOrder(orderBook, "ticker1")
+
+    #     self.assertEqual(orderBook.sellOrders, None, "sell ticker should be matched and thus gone")
+
+    #     self.assertEqual(orderBook.buyOrders.OrderType, "BUY", 'the first buy order insert wrongly. (ordertype)')
+    #     self.assertEqual(orderBook.buyOrders.TickerSymbol, "ticker1", 'the first s order insert wrongly. (tickersymbol)')
+    #     self.assertEqual(orderBook.buyOrders.Qty, 10, 'the first buy order insert wrongly. (quantity)')
+    #     self.assertEqual(orderBook.buyOrders.Price, 50.0, 'the first buy order insert wrongly. (price)')
+
 if __name__ == '__main__':
     unittest.main()
